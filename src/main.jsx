9b15134281d3c93659df97ed20bc918038db7978
Lucas Alcantara <lcaalcantara@gmail.com>
2023-11-06 16:12:56 -0300

    signin page complete

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import MainRoutes from './routes';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.css";
+import MainRoutes from "./routes";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <MainRoutes />
-        </BrowserRouter>
-    </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <MainRoutes />
+    </BrowserRouter>
+  </React.StrictMode>
+);
--- src/pages/SingIn/index.jsx
+++ src/pages/SingIn/index.jsx
@@ -1,12 +1,44 @@
-import './style.css'
+import "./style.css";
+import Logo from "../../assets/logo.svg";
 
 function SignIn() {
+  return (
+    <div className="container-signin">
+      <img src={Logo} alt="logo" className="logo" />
 
-    return (
-        <div>
-            <h1>SignIn</h1>
+      <div className="content-signin">
+        <div className="left">
+          <h1>
+            Controle suas <span>finanças</span>, sem planilha chata.
+          </h1>
+
+          <h3>
+            Organizar suas finanças nunca foi tão fácil, com o DINDIN, você tem
+            tudo num único lugar e em um clique de distância
+          </h3>
+
+          <button className="btn-purple btn-big">Cadastre-se</button>
         </div>
-    )
+        <div className="right">
+          <form>
+            <h2>Login</h2>
+
+            <div className="container-inputs">
+              <label>Email</label>
+              <input type="text" name="email" />
+            </div>
+
+            <div className="container-inputs">
+              <label>Senha</label>
+              <input type="password" name="password" />
+            </div>
+
+            <button className="btn-purple btn-big">Entrar</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default SignIn
+export default SignIn;
